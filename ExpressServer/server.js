5c116631143f0798ed9a70f8d8294f41c39b1850
Alex Tzeng <fate412460@gmail.com>
2016-11-26 13:27:05 +0800

    準備好了要丟給前端的.json，接下來處理前端

--- ExpressServer/server.js
+++ ExpressServer/server.js
@@ -1,6 +1,10 @@
-var keyword = '';
+var outputFrontEndJSON;
+
+// ============================================================ (=x60)
+
+var keyword = 'Hack';
 var host = '';
-var type = ['Conf'];
+var type = [];
 if (type == "") {
     console.log('不限類別');
     type = ['前端', '後端', 'Conf', '校園&社團', '軟體', '程式語言', 'app', '工作', '遊戲', '數據分析&AI', '資安', '開發', '作業系統', '硬體', '社群', '網路', '影像處理'];
@@ -31,51 +35,70 @@ var mongodbModule = require('mongodb');
 var MongoClient = mongodbModule.MongoClient;
 var url = 'mongodb://localhost:27017/Events-Mongo-Database';
 
-MongoClient.connect(url, function(err, db) {
-    if (err) {
-        console.log('Error Connecting to Datebase : ' + err);
-    } else {
-        console.log('Success Connected at : ' + url);
-    }
+var queryAtDatabase = function(callback) {
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+            console.log('Error Connecting to Datebase : ' + err);
+        } else {
+            console.log('Success Connected at : ' + url);
+        }
 
-    var eventsCollection = db.collection('EventsCollection');
+        var eventsCollection = db.collection('EventsCollection');
 
-    var querySyntaxObject = {
-        $and: [{
-            $or: [{
-                    'title': {
-                        '$regex': '.*' + keyword + '.*',
-                        '$options': 'i'
+        var querySyntaxObject = {
+            $and: [{
+                $or: [{
+                        'title': {
+                            '$regex': '.*' + keyword + '.*',
+                            '$options': 'i'
+                        }
+                    },
+
+                    {
+                        'description': {
+                            '$regex': '.*' + keyword + '.*',
+                            '$options': 'i'
+                        }
                     }
-                },
-
-                {
-                    'description': {
-                        '$regex': '.*' + keyword + '.*',
-                        '$options': 'i'
-                    }
+                ]
+            }, {
+                'host': {
+                    '$regex': '.*' + host + '.*',
+                    '$options': 'i'
                 }
-            ]
-        }, {
-            'host': {
-                '$regex': '.*' + host + '.*',
-                '$options': 'i'
-            }
-        }, {
-            'type': {
-                $in: type
-            }
-        }]
-    };
+            }, {
+                'type': {
+                    $in: type
+                }
+            }]
+        };
 
-    console.log('query text is ' + querySyntaxObject['title']);
+        console.log('query text is ' + querySyntaxObject['title']);
 
-    eventsCollection.find(querySyntaxObject).toArray(function(err, result) {
-        console.log(JSON.stringify(result, null, 2));
-        console.log('Search Found ' + result.length + ' Items !');
+        eventsCollection.find(querySyntaxObject).toArray(function(err, result) {
+            console.log(JSON.stringify(result, null, 2));
+            console.log('Search Found ' + result.length + ' Items !');
+            outputFrontEndJSON = result;
+            callback();
+        });
+
+        //Close connection
+        db.close();
+
     });
+}
 
-    //Close connection
-    db.close();
+// ============================================================ (=x60)
 
-});
+var showOutputFrontEndJSON = function() {
+    for (var i = 0; i < outputFrontEndJSON.length; i++) {
+        delete outputFrontEndJSON[i]['_id'];
+        outputFrontEndJSON[i]['fee'] = parseInt(outputFrontEndJSON[i]['fee']);
+        outputFrontEndJSON[i]['number_of_people'] = parseInt(outputFrontEndJSON[i]['number_of_people']);
+    }
+    console.log('要輸出到前端的JSON為' + JSON.stringify(outputFrontEndJSON, null, 2));
+}
+
+// ============================================================ (=x60)
+
+queryAtDatabase(showOutputFrontEndJSON);
